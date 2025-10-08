94327a7adbdab84a678bf3de04ec9893ba5c1fcd
AliKhachab <a.khachab620@gmail.com>
2025-10-08 17:26:41 -0400

    changed some colors on the log in page so things are actually visible

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -4,27 +4,27 @@ export default function Login() {
   return (
 <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
+    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-700">Sign in to your account</h2>
   </div>
 
   <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form action="#" method="POST" className="space-y-6">
       <div>
-        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">Email address</label>
+        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-700">Email address</label>
         <div className="mt-2">
-          <input id="email" type="email" name="email" required autoComplete="email" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+          <input id="email" type="email" name="email" required autoComplete="email" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-indigo-800 outline-1 -outline-offset-1 outline-indigo-500 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Enter your email" />
         </div>
       </div>
 
       <div>
         <div className="flex items-center justify-between">
-          <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">Password</label>
+          <label htmlFor="password" className="block text-sm/6 font-medium text-gray-700">Password</label>
           <div className="text-sm">
             <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</a>
           </div>
         </div>
         <div className="mt-2">
-          <input id="password" type="password" name="password" required autoComplete="current-password" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+          <input id="password" type="password" name="password" required autoComplete="current-password" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-indigo-800 outline-1 -outline-offset-1 outline-indigo-500 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Enter your password" />
         </div>
       </div>
 
@@ -33,7 +33,7 @@ export default function Login() {
       </div>
     </form>
 
-    <p className="mt-10 text-center text-sm/6 text-gray-400">
+    <p className="mt-10 text-center text-sm/6 text-gray-500">
       Don&apos;t have an account? {' '}
       <a href="/register" className="font-semibold text-indigo-400 hover:text-indigo-300">Register here.</a>
     </p>
