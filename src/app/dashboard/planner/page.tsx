a8c16425a7fa41a872a2c0ecf0eae3a1035e2cc5
jli <jligh145@gmail.com>
2025-10-09 08:07:44 -0400

    basic demo of retrieving user object in nested protected pages

--- src/app/dashboard/planner/page.tsx
+++ src/app/dashboard/planner/page.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
 
-export default function PlannerPage() {
+export default async function PlannerPage() {
+  // const supabase = await createClient()
+  // const { data: { user }, } = await supabase.auth.getUser()
+  // if (!user) {
+  //   return <p>Unauthorized</p>
+  // }
+
+  const supabase = await createClient()
+  
+    const { data, error } = await supabase.auth.getUser()
+    if (error || !data?.user) {
+      redirect('/login')
+    }
   return (
-      <div className="pt-24">
-      Planner
+    <div className="pt-24">
+      <p>Planner: Hello {data.user.email}</p>
+      <p>ID: {data.user.id}</p>
+      
     </div>
   )
 }
