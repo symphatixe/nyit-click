5aa5fb58a02a5b39d9d7389f7873efc3b180cb27
AliKhachab <a.khachab620@gmail.com>
2025-10-08 22:15:19 -0400

    commit to force pull from alan's code, do not worry abt this

---
+++ src/app/auth/forget-pass/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ForgotPassword from '../../components/forgot_password';
+
+export default function ForgotPasswordPage() {
+  return (
+    <div>
+      <ForgotPassword />
+    </div>
+  )
+}
--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -20,7 +20,7 @@ export default function Login() {
         <div className="flex items-center justify-between">
           <label htmlFor="password" className="block text-sm/6 font-medium text-gray-700">Password</label>
           <div className="text-sm">
-            <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</a>
+            <a href="/auth/forget-pass" className="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</a>
           </div>
         </div>
         <div className="mt-2">
