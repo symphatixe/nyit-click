688cd218c8ba7854494168430336e401995dff61
David Cano <davecano.244@gmail.com>
2025-10-09 00:27:57 -0400

    Created user profile pic that requires displays their initials

--- src/app/components/profilePicture.tsx
+++ src/app/components/profilePicture.tsx
@@ -1,1 +1,21 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+
+const ProfilePic: React.FC = () => {
+    const [username, setUserName] = useState("Helen Gu");
+
+    const getInitials = (name: string) =>
+        name
+            .split(" ")
+            .map(word => word[0]?.toUpperCase())
+            .join("");
+
+     return (
+    <div className="flex items-center justify-center w-25 h-25 rounded-full border-3 border-[#002D72] bg-gray-50">
+      <span className="text-4xl font-semibold text-[#002D72]">
+        {getInitials(username)}
+      </span>
+    </div>
+  );
+};
+export default ProfilePic;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import WelcomeHeader from "./components/homepageHeader";
+import ProfilePic from "./components/profilePicture";
 export default function Home() {
   return (
-     <div>
-      <WelcomeHeader/>
+     <div className="flex gap-70">
+      <WelcomeHeader/> <ProfilePic/>
     </div>
   );
 
