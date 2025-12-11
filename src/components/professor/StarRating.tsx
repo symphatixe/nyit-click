e5cc634da78aa5b06467a709bd63fd2867c50fbd
vadim <symphatixe@gmail.com>
2025-12-10 22:58:42 -0500

    Update StarRating.tsx
    -fixed starRating issue to generate stars from each student

--- src/components/professor/StarRating.tsx
+++ src/components/professor/StarRating.tsx
@@ -14,7 +14,7 @@ export default function StarRating({
 	return (
 		<div className={`flex items-center gap-2 ${className}`}>
 			<div className="flex gap-1">
-				{[new Array(5)].map((_, i) => (
+				{Array.from({ length: 5 }).map((_, i) => (
 					<Star
 						key={i + 1}
 						size={size}
